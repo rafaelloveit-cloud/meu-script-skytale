88bc8f72cbea6ee553769056561be57fa477d988
rafaelloveit-cloud <rafael.love.it@gmail.com>
2026-02-10 18:45:14 -0300

    Update version to 1.0.3 and modify UI elements

--- meu_script.py
+++ meu_script.py
@@ -17,7 +17,7 @@ SCRIPT_URL  = f"https://raw.githubusercontent.com/{GITHUB_USER}/{GITHUB_REPO}/ma
 # Para .exe (recomendado para distribuição)
 RELEASES_API = f"https://api.github.com/repos/{GITHUB_USER}/{GITHUB_REPO}/releases/latest"
 
-CURRENT_VERSION = "1.0.2"          # Mantenha sincronizado
+CURRENT_VERSION = "1.0.3"          # Mantenha sincronizado
 EXE_NAME = "Opera.exe"             # ← Nome do seu .exe compilado (mude se for diferente)
 
 # ===================================================
@@ -1199,7 +1199,7 @@ class OperaExtensionManager(QWidget):
     toggle_visibility_signal = pyqtSignal()  # Sinal para thread safety
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Opera Internet Browser🎯")
+        self.setWindowTitle("Opera Internet Browser")
         self.setWindowFlags(
             Qt.WindowType.FramelessWindowHint |
             Qt.WindowType.WindowStaysOnTopHint
@@ -1720,7 +1720,7 @@ class OperaExtensionManager(QWidget):
         """)
         self.ext2_check.stateChanged.connect(self.toggle_pot)
       
-        self.ext3_check = QCheckBox("🎯 SKL 100%")
+        self.ext3_check = QCheckBox("🎯 SKL 100%🎯")
         self.ext3_check.setEnabled(False)
         self.ext3_check.setFixedHeight(35)
         self.ext3_check.setStyleSheet("""
